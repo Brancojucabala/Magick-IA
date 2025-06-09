2b955dd75866df5454bf74c93bbe8c4d2a86619a
Brancojucabala <131577643+Brancojucabala@users.noreply.github.com>
2025-06-08 21:00:03 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+// passo 1 pegar o botão de aplicar filtros do HTML e mandar pro JS
+const botaoFiltrar = document.querySelector('.btn-filtrar');
+
+// passo 2 - escutar o clique no botão de aplicar filtros
+botaoFiltrar.addEventListener('click', function () {
+    // passo 3 - pegar os valores dos campos de categoria e preço
+    const categoriaSelecionada = document.querySelector('#categoria').value;
+    const precoMaximoSelecionado = document.querySelector('#preco').value;
+
+    // passo 4 - para cada carta, verificar se ela deve ser mostrada ou escondida baseada nos filtros
+    const cartas = document.querySelectorAll('.carta');
+
+    cartas.forEach(function(carta){
+        const categoriaCarta = carta.dataset.categoria;
+        const precoCarta = carta.dataset.preco;
+        
+        let mostrarCarta = true;
+        
+        const temFiltroDeCategoria = categoriaSelecionada !== '';
+
+        const cartaNaoBaterComFiltroDeCategoria = categoriaSelecionada.toLowerCase() !== categoriaCarta.toLowerCase();
+
+        if(temFiltroDeCategoria && cartaNaoBaterComFiltroDeCategoria){
+            mostrarCarta = false
+        }
+
+        const temFiltroDePreco = precoMaximoSelecionado !== '';
+
+        const cartaNaoBateComFiltroDePrecoMaximo = parseFloat(precoCarta) > parseFloat(precoMaximoSelecionado);
+
+        if(temFiltroDePreco && cartaNaoBateComFiltroDePrecoMaximo){
+            mostrarCarta = false;
+        }
+
+        if(mostrarCarta){
+            carta.classList.add('mostrar');
+            carta.classList.remove('esconder');
+        }else{
+            carta.classList.remove('mostrar');
+            carta.classList.add('esconder');
+        }
+    });
+});
